fa13fc106cf22f7e2bd4a7436c630a429308979f
nazhifsetya27 <123853230+nazhifsetya27@users.noreply.github.com>
2025-07-29 21:38:26 +0700

    Add PolitenessSlider component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import InputSection from './InputSection.jsx'
+import PolitenessSlider from './PolitenessSlider.jsx'
 
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto mt-10 px-4">
+    <div className="max-w-screen-md mx-auto mt-10 px-4 space-y-6">
       <InputSection />
+      <PolitenessSlider />
     </div>
   )
 }
